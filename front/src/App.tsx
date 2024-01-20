0cf77d15030e3a9e308a3b487929fbe53f6ebc0e
matheussASR <rm551466@fiap.com>
2024-01-20 11:11:00 -0300

    feat: adicionando dependencias e rotas

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,12 @@
+import Router from './routes';
+import { ToastContainer } from 'react-toastify';
+
 function App() {
-  
 
   return (
     <>
-      
+      <Router />
+      <ToastContainer/>
     </>
   )
 }
---
+++ front/src/routes.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <Routes>
+      <Route path='/' element={} />
+    </Routes>
+  );
+};
+
+export default Router;
