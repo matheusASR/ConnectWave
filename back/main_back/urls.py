502006858649e50ff558c3cb36ca52433c0aac10
matheussASR <rm551466@fiap.com>
2024-01-23 12:56:52 -0300

    feat: adicionando urls e serializers

--- back/main_back/urls.py
+++ back/main_back/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("api/", include("users.urls")),
+    path("api/", include("posts.urls")),
+    path("api/", include("comments.urls")),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
 ]
--- back/posts/models.py
+++ back/posts/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 class Post(models.Model):
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="posts")
-    content = models.TextField()
+    content = models.TextField(blank=True) 
+    image = models.ImageField(upload_to='post_images/', blank=True, null=True) 
+    caption = models.CharField(max_length=255, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.user.username} - {self.created_at}"
+
 
 
